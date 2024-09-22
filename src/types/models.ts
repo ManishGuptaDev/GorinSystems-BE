cea42172fa14072882fdb85f3cc5062872ef61d1
Manish Gupta <manish.gta1@gmail.com>
2024-09-22 22:19:03 +0530

    update

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,70 +1,16 @@
-export enum ChannelType {
-  EMAIL = "EMAIL",
-  SMS = "SMS"
-}
-
-export type Customer = {
-  id: string;
-  fName: string;
-  lName: string;
-  email: string;
-  emailValidated: boolean;
-  requireReLogin?: boolean;
-  failedLoginAttempts?: number;
-  cannotLoginUntil?: Date;
-  active: boolean;
-  deleted: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-  organizations?: Organization[];
-};
-
-export type Organization = {
-  id: string;
-  customerId?: string;
-  customer?: Customer;
-  accounts?: Account[];
-  users?: User[];
-};
-
-export type Account = {
-  id: string;
-  organizationId: string;
-  authPrimaryToken: string;
-  authSecondaryToken?: string;
-  Organization?: Organization;
-};
-
 export type User = {
-  id: string;
-  organizationId: string;
-  fName: string;
-  lName: string;
-  role: string;
-  Organization?: Organization;
-};
-
-export type VerifyServices = {
   id: number;
-  sid: string;
-  name: string;
-  channel?: ChannelType;
-  notes?: string;
+  surfaceTagId: string;
   createdAt: Date;
-  updatedAt: Date;
-  verifyServicesUsage?: VerifyServicesUsage[];
 };
 
-export type VerifyServicesUsage = {
+export type Event = {
   id: number;
-  verifyServicesId: number;
-  emailToVerify: string;
-  otp: string;
-  generatedOn: Date;
-  expireIn: number;
-  verified: boolean;
-  cost: number;
-  VerifyServices?: VerifyServices;
+  name: string;
+  metadata: object;
+  surfaceTagId: string;
+  visitorId: string;
+  createdAt: Date;
 };
 
 export type VariantType = {
