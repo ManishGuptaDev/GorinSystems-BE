de5abeb2a6d12f7d6707c169a82ddbabd877921a
Manish Gupta <manish.gta1@gmail.com>
2024-09-22 22:35:03 +0530

    fix build issue

--- src/data/repositories/interfaces/IVerifyRepository.ts
+++
@@ -1,7 +0,0 @@
-import { IRead } from "./IRead";
-
-import { VerifyServicesUsage } from "@/types/models";
-
-export interface IVerifyRepository extends IRead<VerifyServicesUsage> {
-   createOtp(item: VerifyServicesUsage): Promise<boolean>;
-}
